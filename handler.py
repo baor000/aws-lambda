eb8848880894d7edfe044dcb8082114faaca47f5
it.johan <it.johan@voltrans.org>
2023-07-01 11:14:56 +0700

    read file excel and return value

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import json
+import gspread
 
 
 def hello(event, context):
@@ -6,5 +7,20 @@ def hello(event, context):
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
     }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
+    try:
+        gc = gspread.service_account(filename="credentials/credentials.json")
+        sheet = gc.open_by_url(event.get("url",""))
+        worksheet = sheet.worksheet(event.get("name",""))
+        col = worksheet.row_values(1)
+        data2 = worksheet.get_all_records()
+        for i,rec in enumerate(data2):
+            rec["id_s"] = i 
+        result_event = {
+            "url":event.get("url"),
+            "name": event.get("name"),
+            "col": col,
+            "rows": data2,
+        } 
+        return {"statusCode": 200, "body": json.dumps(result_event)}
+    except Exception as e:
+        return {"statusCode": 500, "body": json.dumps({'message': str(e)})}
